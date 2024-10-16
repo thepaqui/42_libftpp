42e71169e2c8d38982ab8d7e76b1edc8390f8f0f
thepaqui <theopaquier2@gmail.com>
2024-10-16 14:50:14 +0200

    Started Timer

---
+++ bonus/timer/main.cpp
@@ -0,0 +1,7 @@
+#include "timer.hpp"
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
---
+++ bonus/timer/timer.hpp
@@ -0,0 +1,11 @@
+#ifndef LIBFTPP_TIMER_HPP
+# define LIBFTPP_TIMER_HPP
+
+# include <exception>
+# include <chrono>
+
+class Timer {
+
+};
+
+#endif
